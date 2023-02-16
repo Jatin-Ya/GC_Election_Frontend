abe06b32d022a96ef6e3d63d3ce9c72966d1c528
Jatin Yadav <jatinyadav20032003@gmail.com>
2023-02-16 12:04:09 +0530

    Frontend fixed

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,7 @@
 const HOSTEL = "ME"
 const HOSTEL_NAME = "Mechanical"
 const SERVER_URL = "https://gc-election-backend.onrender.com"
+// const SERVER_URL = "https://localhost:8080"
 const CLIENT_ID = "200338719271-juuspjad20frhr0l2g3mce4ovphobrhk.apps.googleusercontent.com"//"468793858089-a53ggo3iq2a6hn8skl1fabr6jgjdev1n.apps.googleusercontent.com"//"314479029565-i18bqgk3ir82rj8sse8ni6tf2va5strg.apps.googleusercontent.com"
 
 module.exports = {
